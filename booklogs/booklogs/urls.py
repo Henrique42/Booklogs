e47ce6baf07d60db591cca185b2aab14c3a72db2
Henrique42 <bhenrique.arkh@gmail.com>
2025-08-12 16:27:01 -0300

    Listagem de Livros e página de detalhes

--- booklogs/booklogs/urls.py
+++ booklogs/booklogs/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-import reviews.views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', reviews.views.index),
-    path('book-search/', reviews.views.book_search)
+    path('', include('reviews.urls'))
 ]
