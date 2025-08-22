d9a805123c0770e97555f1883db8946782514b8a
Henrique42 <bhenrique.arkh@gmail.com>
2025-08-22 11:29:01 -0300

    Added forms for Reviews and Publishers (chapter 7)

--- booklogs/reviews/forms.py
+++ booklogs/reviews/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Publisher, Review
 
 # Book search form
 class SearchForm(forms.Form):
@@ -13,4 +14,16 @@ class SearchForm(forms.Form):
     # "Fields to be search" selector
     search_in = forms.ChoiceField(required=False, choices=SEARCH_CHOICES)
 
-    
+# Publisher form for creation and updates
+class PublisherForm(forms.ModelForm):
+    class Meta:
+        model = Publisher
+        fields = ("name", "website", "email")
+
+# Review form for creation and updates
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ("content", "rating", "creator")
+
+    rating = forms.IntegerField(min_value=0, max_value=5)
--- booklogs/reviews/urls.py
+++ booklogs/reviews/urls.py
@@ -4,5 +4,9 @@ from . import views
 # "reviews" app urls
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
-    path('book/<int:pk>/', views.book_detail, name='book_detail')
+    path('books/<int:pk>/', views.book_detail, name='book_detail'),
+    path('publishers/<int:pk>/', views.publisher_edit, name="publisher_edit"),
+    path('publishers/new/', views.publisher_edit, name="publisher_create"),
+    path('books/<int:book_pk>/reviews/new/', views.review_edit, name='review_create'),
+    path('books/<int:book_pk>/reviews/<int:review_pk>/', views.review_edit, name='review_edit'),
 ]
